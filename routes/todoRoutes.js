9dd694e5f28c80e7439c9ce2ecbd765d1bf88ad7
Yusuf Oztoprak <147493350+YusufOztoprak@users.noreply.github.com>
2025-03-07 20:12:37 +0300

    Amend

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const taskController = require('../controllers/taskController');
+const taskController = require("../controllers/taskController");
 
-router.get("/",taskController.getTasks);
-router.post("/",taskController.createTask);
-router.put("/:id",taskController.updateTask);
-router.delete("/:id",taskController.deleteTask);
+
+router.get("/api/tasks", taskController.getTasks);
+router.post("/api/tasks", taskController.createTask);
+router.put("/api/tasks/:id", taskController.updateTask);
+router.delete("/api/tasks/:id", taskController.deleteTask);
 
 module.exports = router;
